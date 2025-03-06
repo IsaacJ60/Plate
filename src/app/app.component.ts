1c7764446a0ebc7fa6b0e9b234b8678d4ae730d2
savcode066 <savmath06@gmail.com>
2025-03-06 11:01:22 -0500

    setting up db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 // src/app/app.component.ts
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TodoComponent } from './components/todo/todo.component'; // Correct path to TodoComponent
+import { HomePageComponent } from './features/home-page/home-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    TodoComponent // Import TodoComponent here
+    HomePageComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
